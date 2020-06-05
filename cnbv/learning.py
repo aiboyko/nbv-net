7888afd0005b90b2e65f8f8b4faff885d96aedf4
Dmitrii Smirnov <dmitrii.smirnov@phystech.edu>
2020-06-05 15:26:26 +0300

    1.Train and test functions were moved into .py file. 2. Writing loss and accuracy into logs files

--- cnbv/learning.py
+++ cnbv/learning.py
@@ -1,5 +1,8 @@
-# Validation Function
+# Training and testing functions
 import torch
+import time
+import sys
+import os
 
 def validation(model, testloader, criterion, device='cpu'):
     test_loss = 0
@@ -20,54 +23,105 @@ def validation(model, testloader, criterion, device='cpu'):
     accuracy = N_equal / N
     return test_loss, accuracy
 
-def train(model, optimizer, train_dataloader,  test_dataloader, criterion, device='cpu', calculate_eval=False, epochs=400):
 
-    # Training loop
+def test(model, testloader, criterion):
+    test_loss = 0
+    accuracy = 0
+    for sample in testloader:
+        
+        # get sample data: images and ground truth keypoints
+        grids = sample['grid']
+        nbvs = sample['nbv_class']
+        
+        # convert images to FloatTensors
+        grids = grids.type(torch.FloatTensor)
+        
+        # wrap them in a torch Variable
+        grids = Variable(grids)
+        grids = grids.to(device)
+        
+        # wrap them in a torch Variable
+        nbvs = Variable(nbvs)
+        nbvs = nbvs.to(device)
+        
+        output = model.forward(grids)
+        test_loss += criterion(output, nbvs).item()
+        
+        # for log.  ps = torch.exp(output)
+        equality = (nbvs.data == output.max(dim=1)[1])
+        accuracy += equality.type(torch.FloatTensor).mean()
+    
+    return test_loss, accuracy
+
+
+
+def train(model, optimizer, train_dataloader, test_dataloader, criterion, device='cpu', calculate_eval=False,
+          epochs=400):
+    
     running_loss = 0
-    save_after = 100
-
+    save_after = 1
+    
     history_epoch = []
     history_train_loss = []
-    history_validation_loss = []
+    history_test_loss = []
     history_train_accuracy = []
-    history_validation_accuracy = []
-
-    import time
+    history_test_accuracy = []
+    
+    path_to_log = os.path.join(sys.path[0], "../log/")
+    
     for e in range(epochs):
-        model.train()
         tic = time.time()
-        cumulative_loss = 0
         for i, (X, y) in enumerate(train_dataloader):
-            # print(X.shape)
             optimizer.zero_grad()
-            model.to(device)
-            output = model(X.to(device).view(-1, 1, 32, 32, 32))
-            loss = criterion(output, y.to(device).T[0])
+            output = model(X)
+            loss = criterion(output, y.flatten())
             loss.backward()
-            cumulative_loss += loss
             optimizer.step()
-
-        history_train_loss.append(cumulative_loss.detach().cpu().numpy())
-        history_epoch.append(e)
-
-
-        if calculate_eval == True:
-            model.eval()
-            with torch.no_grad():
-                train_loss, train_accuracy = validation(model, train_dataloader, criterion, device)
-                val_loss, val_accuracy = validation(model, test_dataloader, criterion,device)
-                train_accuracy = train_accuracy / len(train_dataloader)
-                val_accuracy = val_accuracy / len(test_dataloader)
-
-                print("Epoch: {}/{}.. ".format(e+1, epochs),
-                    "Training Loss: {:.3f}.. ".format(train_loss),
-                    "Val. Loss: {:.3f}.. ".format(val_loss),
-                    "Train Accuracy: {:.3f}".format(train_accuracy),
-                    "Val. Accuracy: {:.3f}".format(val_accuracy))
+        
+        model.eval()
+        with torch.no_grad():
+            train_loss, train_accuracy = test(model, train_dataloader, criterion, device)
+            train_accuracy = train_accuracy / len(train_dataloader)
             
-            history_validation_loss.append(val_loss)
+            history_train_loss.append(train_loss)
             history_train_accuracy.append(train_accuracy)
-            history_validation_accuracy.append(val_accuracy)
+            
+            print("Epoch: {}/{}.. ".format(e+1, epochs),
+                  "Training Loss: {:.3f}.. ".format(train_loss),
+                  "Training Accuracy: {:.3f}.. ".format(train_accuracy))
+                
+            if calculate_eval==True:
+                test_loss, test_accuracy = test(model, test_dataloader, criterion, device)
+                test_accuracy = test_accuracy / len(test_dataloader)
+                              
+                history_test_loss.append(test_loss)
+                history_test_accuracy.append(test_accuracy)
+                                      
+                print("Epoch: {}/{}.. ".format(e+1, epochs),
+                        "Test Loss: {:.3f}.. ".format(val_loss),
+                        "Test Accuracy: {:.3f}".format(val_accuracy))
+                  
+                if (e % save_after) == 0:
+                    np.save(path_to_log + 'train_loss', history_train_loss)
+                    np.save(path_to_log + 'train_accuracy', history_train_accuracy)
+                    torch.save(net.state_dict(), path_to_log + 'weights' + '.pth')
+                                                  
+                    if calculate_eval==True:
+                        np.save(path_to_log + 'test_loss', history_test_loss)
+                        np.save(path_to_log + 'test_accuracy', history_test_accuracy)
+                                                              
+        toc = time.time()
+        print('time per epoch = ', toc - tic)
+        print('-----')
 
-        print(time.time() - tic)
-        print('---')
+
+
+
+
+
+
+
+
+
+
+
